b7ce26ed167652ee0373d3efbb0d2ec9ddf47aa9
savelyevresearch <savelyevresearch@protonmail.com>
2024-10-31 22:41:55 +0600

    The config package has been written

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"url-shortener/internal/config"
+)
+
 func main() {
 	// TODO: init config: cleanenv
+
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
 
 	// TODO: init logger: slog
 
